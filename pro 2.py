007b7116c55185d8616c764b0975520dbe6ec4b8
sreeyuvanash <75154505+sreeyuvanash@users.noreply.github.com>
2021-07-27 17:29:28 +0530

    Add files via upload

---
+++ pro 2.py
@@ -0,0 +1,8 @@
+ 
+first_num = int(input("enter the first number"))
+second_num = int(input("enter the second number"))
+total = first_num+ second_num
+print("total is :",total)
+print("total of",first_num,"and",second_num,"=",total)
+print("total of {} and {} is {}".format (first_num,second_num,total))
+
